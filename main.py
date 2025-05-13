1608987435e804e3a19f48c3c551164d2ba79abf
voltiods3 <voltio.dibujos@gmail.com>
2025-05-13 11:33:34 -0400

    creacion de variables de decision

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import pandas as pd
+from os import path
 from gurobipy import Model, GRB
-
+import gurobipy as gp
+## encontrar J e I
+RUTA_CSV = "./csv/"
 def cargar_datos():
+    data ={
+
+    }
     """
     Esta función debe leer los 9 archivos .csv de la instancia y devolver
     un diccionario con todos los parámetros necesarios para construir el modelo.
     """
+
     raise NotImplementedError("Implementa esta función para cargar los datos.")
 
 def construir_modelo(data):
+    model = gp.Model() # crea modelo
+    I = data["I"]
+    J = data["J"]
+
+    x_i = model.addVars(I,lb=0.0, vtype=GRB.CONTINUOUS, name="x_i")
+    y_j = model.addvars(J, lb=0.0, vtype=GRB.CONTINUOUS, name="y_j")
+    w_i = model.addVars(I,vtype=GRB.BINARY, name="w_i")
+
+    model.update()
     """
     Esta función debe construir el modelo de optimización utilizando Gurobi
     y los datos provistos en el diccionario `data`.
